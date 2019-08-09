8c7528b49cc3a1d47e9eca386f46bbde10497bc6
Tyler Berrett <btylerberrett@gmail.com>
2019-08-09 09:52:47 -0600

    make save button work

--- app/src/main/java/com/example/sprintchallenge/application/ListOfMovies.kt
+++ app/src/main/java/com/example/sprintchallenge/application/ListOfMovies.kt
@@ -4,10 +4,20 @@ import android.app.Application
 import com.example.sprintchallenge.model.movieListItem
 
 class ListOfMovies: Application() {
-    val movies = ArrayList<movieListItem>()
 
-    fun addMovie(movie: movieListItem) {
-        movies.add(movie)
+    companion object{
+        var movies = ArrayList<movieListItem>()
+
+        fun addMovie(movie: movieListItem) {
+            movies.add(movie)
+        }
+
+
     }
+
+
+
+
+
 
 }
--- app/src/main/java/com/example/sprintchallenge/ui/EditPage.kt
+++ app/src/main/java/com/example/sprintchallenge/ui/EditPage.kt
@@ -1,14 +1,28 @@
 package com.example.sprintchallenge.ui
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sprintchallenge.R
+import com.example.sprintchallenge.application.ListOfMovies
+import com.example.sprintchallenge.model.movieListItem
+import kotlinx.android.synthetic.main.activity_edit_page.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class EditPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_page)
+
+        val movieText = movie_text.text.toString()
+        val seenMovie = switch_seen.isChecked
+
+
+        button_add.setOnClickListener {
+            ListOfMovies.addMovie(movieListItem(movieText, seenMovie, ListOfMovies.movies.size))
+
+        }
 
 
     }
