5bb2a412080a848776dfc3e4d928d15fad6291f0
Tyler Berrett <btylerberrett@gmail.com>
2019-08-09 09:18:58 -0600

    add ui layout

---
+++ app/src/main/java/com/example/sprintchallenge/ui/EditPage.kt
@@ -0,0 +1,13 @@
+package com.example.sprintchallenge.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sprintchallenge.R
+
+class EditPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_page)
+    }
+}
